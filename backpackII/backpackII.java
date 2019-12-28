046e4b7b6b3f186771a4d0b7ae844b1e55868a3d
chaowen guo <chaowen.guo1@gmail.com>
2019-12-28 08:59:15 -0600

    Create  backpackII.java

---
+++ backpackII/backpackII.java
@@ -0,0 +1,14 @@
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @param V: Given n items with value V[i]
+     * @return: The maximum value
+     */
+    public int backPackII(int m, int[] A, int[] V) {
+        final List<Integer> dp = Collections.nCopies(m + 1, 0).stream().collect(java.util.stream.Collectors.toList());
+        for (int i = 0; i != A.length; ++i)
+            for (int j = m; j != A[i] - 1; --j) dp.set(j, Math.max(dp.get(j), dp.get(j - A[i]) + V[i]));
+        return dp.get(dp.size() - 1);
+    }
+}
