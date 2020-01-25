7c4a6662082c8285b40e7969a615f86063428025
chaowen guo <chaowen.guo1@gmail.com>
2020-01-24 18:43:23 -0600

    Update backpackV.java

--- 563backpackV/backpackV.java
+++ 563backpackV/backpackV.java
@@ -6,9 +6,9 @@ public class Solution {
      */
     public int backPackV(int[] nums, int target) {
         // write your code here
-        final int[] dp = new dp[target + 1];
+        final int[] dp = new int[target + 1];
         dp[0] = 1;
-        Arrays.stream(nums).forEach(item -> java.util.stream.IntStream.range(1, target + 1).boxed().sorted(Comparator.reverseOrder()).forEach(size -> dp[size] += size >= item ? dp[size - item] : 0));
+        Arrays.stream(nums).forEach(item -> java.util.stream.IntStream.range(item, target + 1).boxed().sorted(Comparator.reverseOrder()).forEach(size -> dp[size] += dp[size - item]));
         return dp[dp.length - 1];
     }
 }
