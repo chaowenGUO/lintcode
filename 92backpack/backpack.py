6972494cb01271f2056ebd295e167783228755a3
chaowen guo <chaowen.guo1@gmail.com>
2020-01-24 18:39:48 -0600

    Update backpack.py

--- 92backpack/backpack.py
+++ 92backpack/backpack.py
@@ -7,7 +7,7 @@ class Solution:
     @return: The maximum size
     """
     def backPack(self, m, A):
-        dp = [True, *itertools.repeat(False, m)]
+        dp = [1, *itertools.repeat(0, m)]
         for item in A:
-            for size in range(m, 0, -1): dp[size] |= dp[size - item] if size >= item else False
-        return max(index for index, _ in enumerate(dp) if _ == True)
+            for size in range(m, item - 1, -1): dp[size] |= dp[size - item]
+        return max(index for index, _ in enumerate(dp) if _ == 1)
