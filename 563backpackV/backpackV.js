009b819093e81c829017127cae82febb9c1c80ab
chaowen guo <chaowen.guo1@gmail.com>
2020-01-24 18:44:23 -0600

    Update backpackV.js

--- 563backpackV/backpackV.js
+++ 563backpackV/backpackV.js
@@ -6,6 +6,6 @@
 const backPackV = function (nums, target) {
     const dp = [1, ...Array(target).fill(0)]
     nums.forEach(item => {
-        for (let size = target; !Object.is(size, 0); --size) dp[size] += size >= item ? dp[size - item] : 0})
+        for (let size = target; size > item - 1; --size) dp[size] += dp[size - item]})
     return dp[dp.length - 1]
 }
