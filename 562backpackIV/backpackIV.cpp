1b37644ed77f6ed13514dd24f623251e608e36d8
chaowen guo <chaowen.guo1@gmail.com>
2020-01-24 18:50:37 -0600

    Update backpackIV.cpp

--- 562backpackIV/backpackIV.cpp
+++ 562backpackIV/backpackIV.cpp
@@ -10,7 +10,7 @@ public:
         std::vector<int> dp(target + 1);
         dp.front() = 1;
         for (auto const item: nums)
-            for (std::size_t size{1}; size != target + 1; ++size) dp.at(size) += size >= item ? dp.at(size - item) : 0;
+            for (std::size_t size{item}; size != target + 1; ++size) dp.at(size) += dp.at(size - item);
         return dp.back();
     }
 };
